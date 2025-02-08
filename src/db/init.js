0790c728435de627721e5b06ccd04940f798bbe6
sasnations <rcvtag4@gmail.com>
2025-02-08 14:39:00 -0500

    init.js

--- src/db/init.js
+++ src/db/init.js
@@ -6,7 +6,7 @@ dotenv.config();
 // Create the pool with optimized settings
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 25060,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
